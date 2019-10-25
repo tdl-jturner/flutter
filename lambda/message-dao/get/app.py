bee0ecbd2c014bee9994b9f909ac0fe7fdae1ecf
Josh Turner <turner.josh@gmail.com>
2019-10-25 12:12:04 -0700

    First run of basic message-dao in lambda

---
+++ lambda/message-dao/get/app.py
@@ -0,0 +1,41 @@
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError
+
+def lambda_handler(event, context):
+
+    if not 'payload' in event:
+        return {"StatusCode": 500,"body":"Payload not found"}
+
+    try:
+        response = boto3.resource('dynamodb').Table('Messages').get_item(
+            Key={
+                'id': event['payload']['id']
+            }
+        )
+
+    except ClientError as e:
+        if e.response['Error']['Code'] == "ResourceNotFoundException":
+            return {
+                "statusCode": 404,
+                "body": {
+                    "error":"No record found"
+                }
+            }
+        else:
+            return {
+                "statusCode": 500,
+                "body": {
+                    "error":e.respons['Error']
+                }
+            }
+        print(e.response['Error']['Message'])
+    else:
+        item = response['Item']
+
+        return {
+            "statusCode": 200,
+            "body": {
+                "payload": item
+            }
+        }
---
+++ lambda/message-dao/save/app.py
@@ -0,0 +1,32 @@
+import uuid
+import boto3
+import datetime
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError
+
+def lambda_handler(event, context):
+
+    if not 'payload' in event:
+        return {"StatusCode": 500,"body":"Payload not found"}
+
+    try:
+        message = {
+            "id": (event['payload']['id'] if 'id' in event['payload'] else str(uuid.uuid4())),
+            "message": event['payload']['message'],
+            "createdDttm": (event['payload']['createdDttm'] if 'createdDttm' in event['payload'] else int(datetime.datetime.utcnow().timestamp()))
+        }
+        boto3.resource('dynamodb').Table('Messages').put_item(Item=message)
+    except ClientError as e:
+        return {
+            "statusCode": 500,
+            "body": {
+                "error":e.response['Error']
+            }
+        }
+    else:
+        return {
+            "statusCode": 200,
+            "body": {
+                "payload": message
+            }
+        }
